e039099b95d1403b453bef594a1903b0f0c0c2d6
Avnish Hirani <AHirani@spartaglobal.com>
2019-07-24 16:52:30 +0100

    Second test completed

---
+++ Naan2.py
@@ -0,0 +1,5 @@
+#Test 1 - making_dough() input - wheat and water - expected output dough
+from Naan2_func import *
+print(make_dough("wheat", "water") == "dough")
+#Test 2 - bake_dough() input - dough - expected output = naan
+print(bake_dough("dough") == "naan")
---
+++ Naan2_func.py
@@ -0,0 +1,4 @@
+def make_dough (arg1, arg2):
+    return "dough"
+def bake_dough (arg1):
+    return "naan"
